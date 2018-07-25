416b169c9de3956fd5bf626b72a492fae26c18af
Poitier Stringer <pstringe@e1z3r5p18.42.us.org>
2018-07-24 19:17:32 -0700

    ft_ls has been modularized

--- directory.c
+++ directory.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 17:30:57 by pstringe          #+#    #+#             */
-/*   Updated: 2018/07/24 18:48:14 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/07/24 19:14:13 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,37 +41,36 @@ static int	get_files(t_list **dlst, char *path, t_ops *ops)
 	return (blocks);
 }
 
-static void	break_func()
+static void sort_files(t_list **dlst, t_ops *ops, void **aux)
 {
-	return ;
+	if (!ops->t && !ops->r)
+		ft_lstsort(*dlst, lex, aux, 2);
+	else if (ops->r)
+		ft_lstsort(*dlst, rev, aux, 2);
+	else if (ops->t)
+		ft_lstsort(*dlst, tim, aux, 2);
 }
 
 /*
 **	given a directory, extracts filnames and prints relvant information in propper order
 **	depending on the specified options
 */
+
 void	output_dir(char *path, t_ops *ops)
 {
 	void			*aux[2];	
 	int				blocks;
 	t_list			*dlst;
 
-	if (!ft_strncmp(path, "//nfs/2018/j/jsanders/Day05", 27))
-		break_func();
 	if (ops->R && ft_strncmp(path, ".", ft_strlen(path)))
-		ft_printf("\n\n%s\n", path);
+		ft_printf("\n%s\n", path);
 	dlst = NULL;
 	blocks = get_files(&dlst, path, ops);
 	if (ops->l)
 		ft_printf("total %d\n", blocks);
 	aux[0] = (void*)ops;
 	aux[1] = (void*)path;
-	if (!ops->t && !ops->r)
-		ft_lstsort(dlst, lex, aux, 2);
-	else if (ops->r)
-		ft_lstsort(dlst, rev, aux, 2);
-	else if (ops->t)
-		ft_lstsort(dlst, tim, aux, 2);
+	sort_files(&dlst, ops, aux);
 	ft_lstforeach(dlst, output_stats, aux, 2);
 	aux[0] = NULL;
 	aux[1] = NULL;
