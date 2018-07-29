4dfb03f263f9a1839d1a390157b4d8d6d561a6a8
Poitier Stringer <pstringe@e1z3r5p18.42.us.org>
2018-07-28 19:56:35 -0700

    sort.c normed

--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/24 17:56:20 by pstringe          #+#    #+#             */
-/*   Updated: 2018/07/27 12:00:04 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/07/28 19:56:07 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int		lex(void *a, void *b, void **aux, int len)
 {
-	const char 	*s1;
-	const char 	*s2;
+	const char	*s1;
+	const char	*s2;
 	size_t		l1;
 	size_t		l2;
 
@@ -25,17 +25,16 @@ int		lex(void *a, void *b, void **aux, int len)
 	s2 = (const char*)b;
 	l1 = ft_strlen(s1);
 	l2 = ft_strlen(s2);
-	return(ft_strncmp(s1, s2, (l1 >= l2 ? l1 : l2)));
+	return (ft_strncmp(s1, s2, (l1 >= l2 ? l1 : l2)));
 }
 
 int		tim(void *a, void *b, void **aux, int len)
 {
-	
-	char 	*s1;
-	char 	*s2;
-	time_t		t1;
-	time_t		t2;
-	char		p[512];
+	char	*s1;
+	char	*s2;
+	time_t	t1;
+	time_t	t2;
+	char	p[512];
 
 	if (!aux || !len)
 		aux = NULL;
@@ -43,17 +42,15 @@ int		tim(void *a, void *b, void **aux, int len)
 	s2 = (char*)b;
 	get_path(p, (char*)aux[1], s1);
 	t1 = get_stats(p).st_mtime;
-	//ft_memdel((void**)&p);
 	get_path(p, (char*)aux[1], s2);
 	t2 = get_stats(p).st_mtime;
-	//ft_memdel((void**)&p);
-	return(t1 < t2);
+	return (t1 < t2);
 }
 
 int		rev(void *a, void *b, void **aux, int len)
 {
-	const char 	*s1;
-	const char 	*s2;
+	const char	*s1;
+	const char	*s2;
 	size_t		l1;
 	size_t		l2;
 
@@ -63,6 +60,5 @@ int		rev(void *a, void *b, void **aux, int len)
 	s2 = (const char*)b;
 	l1 = ft_strlen(s1);
 	l2 = ft_strlen(s2);
-	return(ft_strncmp(s2, s1, (l1 >= l2 ? l1 : l2)));
+	return (ft_strncmp(s2, s1, (l1 >= l2 ? l1 : l2)));
 }
-
