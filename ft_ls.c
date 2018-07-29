fb1646e11e7254c827e7f6c9ab084f2ba5387b53
Poitier Stringer <pstringe@e1z3r5p18.42.us.org>
2018-07-28 19:36:07 -0700

    ft_ls.c normed

--- ft_ls.c
+++ ft_ls.c
@@ -6,13 +6,13 @@
 /*   By: pstringe <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/30 18:55:49 by pstringe          #+#    #+#             */
-/*   Updated: 2018/07/28 19:09:12 by pstringe         ###   ########.fr       */
+/*   Updated: 2018/07/28 19:35:40 by pstringe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_ls.h"
 
-void 	init_options(t_ops **ops)
+void	init_options(t_ops **ops)
 {
 	t_ops *o;
 
@@ -25,11 +25,11 @@ void 	init_options(t_ops **ops)
 	*ops = o;
 }
 
-int 	parse_options(char **args, int argn, t_ops *ops)
+int		parse_options(char **args, int argn, t_ops *ops)
 {
 	int	i;
 	int	j;
-	
+
 	if (argn == 1)
 		return (-1);
 	i = 0;
@@ -37,7 +37,6 @@ int 	parse_options(char **args, int argn, t_ops *ops)
 	{
 		j = 0;
 		while (args[i][++j])
-		{
 			if (args[i][j] == 'l')
 				ops->l = 1;
 			else if (args[i][j] == 'a')
@@ -50,15 +49,14 @@ int 	parse_options(char **args, int argn, t_ops *ops)
 				ops->R = 1;
 			else
 				return (die(-3, &args[i][j]));
-		}
 	}
 	return (i);
 }
 
 void	ft_ls(t_ops *ops, char **argv, int argc, int idx)
 {
-	int 		no_of_dirs;
-	int			i;
+	int no_of_dirs;
+	int i;
 
 	no_of_dirs = argc - (idx < 0 ? 1 : idx);
 	if (!ops->R && !no_of_dirs)
@@ -73,7 +71,7 @@ void	ft_ls(t_ops *ops, char **argv, int argc, int idx)
 			recurse(argv[++i], ops);
 }
 
-int	main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	t_ops	*ops;
 	int		i;
